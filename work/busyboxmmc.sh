50315d9b454df8597af6c2352cb6b569ab7caff6
Jonathan Richard Robert Kimmitt <jrrk2@cl.cam.ac.uk>
2018-06-26 08:28:24 +0100

    Separate out NFS and MMC options

---
+++ work/busyboxmmc.sh
@@ -0,0 +1,25 @@
+#!/bin/busybox ash
+/bin/busybox --install -s
+sleep 2
+echo Mounting SD root
+mount -t ext2 /dev/mmcblk0p2 /mnt || (echo Mount failed, dropping to ash)
+echo Mounting proc
+mkdir -p /mnt/proc
+mount -t proc none /mnt/proc
+echo Mounting sysfs
+mkdir -p /mnt/sys
+mount -t sysfs none /mnt/sys
+echo Mounting devtmpfs
+mkdir -p /mnt/dev
+mount -t devtmpfs udev /mnt/dev
+mkdir -p /mnt/dev/pts
+echo Mounting devpts
+mount -t devpts devpts /mnt/dev/pts
+echo Mounting tmpfs
+mkdir -p /mnt/tmp
+mount -t tmpfs tmpfs /mnt/tmp
+mkdir -p /mnt/run
+mount -t tmpfs tmpfs /mnt/run
+mv /bin/busybox /mnt/bin
+echo Executing switch_root
+exec switch_root /mnt /sbin/init
